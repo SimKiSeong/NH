d880b526dc494089dd1e86a41fe735b6669ac0f9
dadadamarine <dadadamarine@naver.com>
2018-06-01 18:57:32 +0900

    third

--- buy/models.py
+++ buy/models.py
@@ -14,35 +14,32 @@ from django.contrib.auth.models import User
 class Album(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField('One Line Description', max_length=100, blank=True)
-    #owner = models.ForeignKey(User, null=True)
+    owner = models.ForeignKey(User, null=True)
 
     class Meta:
         ordering = ['name']
-
+        
     def __str__(self):
         return self.name
-
+        
     def get_absolute_url(self):
         return reverse('photo:album_detail', args=(self.id,))
 
 @python_2_unicode_compatible
 class Photo(models.Model):
-    album = models.ForeignKey(Album , default=0)
-    title = models.CharField(max_length=50 , default=0)
-    image = ThumbnailImageField(upload_to='photo/%Y/%m' , default=0)
-    description = models.TextField('Photo Description', blank=True , default=0)
+    album = models.ForeignKey(Album)
+    title = models.CharField(max_length=50)
+    image = ThumbnailImageField(upload_to='photo/%Y/%m')
+    description = models.TextField('Photo Description', blank=True)
     upload_date = models.DateTimeField('Upload Date', auto_now_add=True)
-    owner = models.ForeignKey(User, null=True, default=0)
-    cnt = models.CharField(max_length=50, default=0)
-    price = models.CharField(max_length=50, default=0)
-    Subscription_ratings=models.CharField(max_length=50 , default=0)
-    purchase_after_sub=models.CharField(max_length=50, default=0)
-
+    owner = models.ForeignKey(User, null=True)
+        
     class Meta:
         ordering = ['title']
-
+        
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('photo:photo_detail', args=(self.id,))
+
